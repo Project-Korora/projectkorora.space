e34127b2e8f01a9dc1017f5bdd24e8c97bf9a092
Albion <albion.pucher@gmail.com>
2025-07-09 17:49:48 +1200

    docs: document skeleton

--- src/app/components/Skeleton.tsx
+++ src/app/components/Skeleton.tsx
@@ -1,4 +1,10 @@
+import React from "react";
 
+/**
+ * Skeleton placeholder for streaming elements or placeholder
+ * @param {React.ComponentProps<"div">} props 
+ * @returns {JSX.Element}
+ */
 export default function Skeleton({className, ...remaining}: React.ComponentProps<"div">) {
     return (
         <div
